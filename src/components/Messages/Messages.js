e977fb49af0d80ec2d34193ef8de1edb0c0a4bb7
Shriram-Sivanandam <ssshriram4@gmail.com>
2021-05-20 21:00:50 +0530

    chat messages

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -2,15 +2,30 @@ import React from "react";
 
 import ScrollToBottom from "react-scroll-to-bottom";
 
-import Message from "./Message/Message";
-
-import "./Messages.css";
+import "./Message.css";
 
 const Messages = ({ messages, name }) => (
-  <div className="bg"></div>
-  // <ScrollToBottom className="messages">
-  //   {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
-  // </ScrollToBottom>
+  <div className="bg">
+    <ScrollToBottom className="messages">
+      <div className="bg">
+        {/* Message sent by the current user */}
+        <div className="messageContainer justifyEnd">
+          <p className="sentText pr-10">name</p>
+          <div className="messageBox backgroundBlue">
+            <p className="messageText colorWhite">message</p>
+          </div>
+        </div>
+
+        {/* message sent by others in the chat room */}
+        <div className="messageContainer justifyStart">
+          <div className="messageBox backgroundLight">
+            <p className="messageText colorDark">message</p>
+          </div>
+          <p className="sentText pl-10 ">name</p>
+        </div>
+      </div>
+    </ScrollToBottom>
+  </div>
 );
 
 export default Messages;
